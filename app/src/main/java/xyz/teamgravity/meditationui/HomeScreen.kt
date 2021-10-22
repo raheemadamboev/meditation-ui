dac5528a89861a9e91a895fcf0ba638691b57b8f
Raheem <raheemideadev@gmail.com>
2021-10-23 01:11:05 +0500

    chip section

--- app/src/main/java/xyz/teamgravity/meditationui/HomeScreen.kt
+++ app/src/main/java/xyz/teamgravity/meditationui/HomeScreen.kt
@@ -1,17 +1,24 @@
 package xyz.teamgravity.meditationui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import xyz.teamgravity.meditationui.ui.theme.ButtonBlue
+import xyz.teamgravity.meditationui.ui.theme.DarkerButtonBlue
 import xyz.teamgravity.meditationui.ui.theme.DeepBlue
+import xyz.teamgravity.meditationui.ui.theme.TextWhite
 
 @Composable
 fun HomeScreen() {
@@ -22,6 +29,7 @@ fun HomeScreen() {
     ) {
         Column {
             GreetingSection(name = "Raheem")
+            ChipSection(chips = listOf("Sweet sleep", "Insomnia", "Depression"))
         }
     }
 }
@@ -54,5 +62,33 @@ fun GreetingSection(
             tint = Color.White,
             modifier = Modifier.size(24.dp)
         )
+    }
+}
+
+@Composable
+fun ChipSection(
+    chips: List<String>
+) {
+    var selectedChipIndex by remember { mutableStateOf(0) }
+
+    LazyRow {
+        items(chips.size) {
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .padding(start = 15.dp, top = 15.dp, bottom = 15.dp)
+                    .clickable {
+                        selectedChipIndex = it
+                    }
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(if (selectedChipIndex == it) ButtonBlue else DarkerButtonBlue)
+                    .padding(15.dp)
+            ) {
+                Text(
+                    text = chips[it],
+                    color = TextWhite
+                )
+            }
+        }
     }
 }

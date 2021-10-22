aed3d0fbeb9be5c7332dd75a58412e606bd20ff6
Raheem <raheemideadev@gmail.com>
2021-10-23 00:54:45 +0500

    greeting section

--- app/src/main/java/xyz/teamgravity/meditationui/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/meditationui/MainActivity.kt
@@ -14,7 +14,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            MeditationUITheme(darkTheme = false) {
+                HomeScreen()
+            }
         }
     }
 }
